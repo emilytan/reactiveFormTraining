d7a380102721ac2dca655d88fd91d278d3aef202
Tan, Emily <eta@softwareag.com>
2021-06-08 16:24:10 +0800

    reactive form training

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,78 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormArray, FormBuilder, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'FormTraining';
+export class AppComponent implements OnInit {
+  locations: string[] = ['Selangor', 'Pahang', 'KL', 'Penang'];
+  empForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    // * Form Builder injection*//
+    // this.empForm = this.fb.group({
+    //   name: 'Name here',
+    //   phoneNumber: '',
+    //   state: '',
+    //   cert: this.fb.group({
+    //     java: false,
+    //     angular: false,
+    //     dotnet: false,
+    //   }),
+    //   workExp: this.fb.array([this.fb.control('')]),
+    // });
+
+    // * Form Group, Form Control, Form Array injection*//
+    this.empForm = new FormGroup({
+      name: new FormControl('Name here', [Validators.required]),
+      phoneNumber: new FormControl('1234'),
+      state: new FormControl('Selangor'),
+      cert: this.empForm = new FormGroup({
+        java: new FormControl(false),
+        angular: new FormControl(true),
+        dotnet: new FormControl(false),
+      }),
+      workExp: new FormArray([
+        new FormControl('experience 1'),
+        new FormControl('experience 2'),
+      ]),
+    });
+  }
+
+  get workExp(): FormArray {
+    return this.empForm.get('workExp') as FormArray;
+  }
+
+  onReactiveSubmit(): any {
+    console.log('this.empForm', this.empForm);
+  }
+
+  addWorkExp(): any {
+    // this.workExp.push(this.fb.control(''));
+    this.workExp.insert(1, new FormControl(''));
+  }
+  removeWorkExp(index: number): any {
+    this.workExp.removeAt(index);
+  }
+
+  selectState(event): any {
+    this.empForm.patchValue({
+      state: event.target.value,
+    });
+  }
+
+  get f() {
+    return this.empForm.controls;
+  }
+
+  forbiddenNameValidator(forbiddenName: string): ValidatorFn {
+    return (control: AbstractControl): {[key: string]: any} | null => {
+      return control.value === forbiddenName ? { forbiddenName: {value: control.value}} : null;
+    };
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroFormComponent } from './hero-form/hero-form.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
